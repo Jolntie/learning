7e7bec6b9c2cf19d0a9d7b47b8cd236df8acdb8f
Jelle <robin@eal.nl>
2024-05-15 15:26:57 +0200

    Creation of files and webApp can use router

--- src/router.js
+++ src/router.js
@@ -1,15 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import CoachDetail from "./pages/coaches/CoachDetail.vue";
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import RequestsRecieved from "./pages/requests/RequestsRecieved.vue";
+import NotFound from './pages/NotFound.vue'
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/coaches', component: null },
-        { path: '/coaches/:id', component: null, children: [
-            { path: '/contact', component: null },
+        { path: '/coaches', component: CoachesList },
+        { path: '/coaches/:id', component: CoachDetail, children: [
+            { path: '/contact', component: ContactCoach },
         ] },
-        { path: '/request', component: null },
-        { path: '/:notFound(.*)', component: null },
+        { path: '/register', component: CoachRegistration },
+        { path: '/request', component: RequestsRecieved },
+        { path: '/:notFound(.*)', component: NotFound },
     ],
 });
 
