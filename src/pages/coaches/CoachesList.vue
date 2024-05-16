50cf85dd36f6305a5fd2cb3b91c4699c7fbd55db
Jelle <robin@eal.nl>
2024-05-16 11:25:37 +0200

    Dummy dataa coaches showing on screen with CoachItem.vue

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,5 +1,10 @@
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue'
+
 export default {
+    components: {
+        CoachItem
+    },
     computed: {
         filteredCoaches() {
             return this.$store.getters['coaches/coaches'];
@@ -20,10 +25,22 @@ export default {
             <router-link to="/register">Register as Coach</router-link>
         </div>
         <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCoaches" :key="coach.id">
-                {{ coach.firstName }}
-            </li>
+            <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+                :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
         </ul>
         <h3 v-else>No coaches found.</h3>
     </section>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
