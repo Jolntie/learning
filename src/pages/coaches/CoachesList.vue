9818fe5349224a7e1f089ee3788d6ec1d49faddd
Jelle <robin@eal.nl>
2024-05-16 11:48:14 +0200

    Base components made and applied in code

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -20,27 +20,30 @@ export default {
         sectie A
     </section>
     <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
-                :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
-        </ul>
-        <h3 v-else>No coaches found.</h3>
+        <base-card>
+            <div class="controls">
+                <base-button mode="outline">Refresh</base-button>
+                <base-button link to="/register">Register as Coach</base-button>
+            </div>
+            <ul v-if="hasCoaches">
+                <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id"
+                    :first-name="coach.firstName" :last-name="coach.lastName" :rate="coach.hourlyRate"
+                    :areas="coach.areas"></coach-item>
+            </ul>
+            <h3 v-else>No coaches found.</h3>
+        </base-card>ß
     </section>
 </template>
 
 <style scoped>
 ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+    list-style: none;
+    margin: 0;
+    padding: 0;
 }
 
 .controls {
-  display: flex;
-  justify-content: space-between;
+    display: flex;
+    justify-content: space-between;
 }
 </style>
