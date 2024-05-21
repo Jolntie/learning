5221b3f073c087869f4becfc239a5053a954ec04
Jelle <robin@eal.nl>
2024-05-21 11:48:15 +0200

    As coach you can see only your requests

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,1 +1,9 @@
-export default {};
+export default {
+    requests(state, _getters, _rootState, rootGetters) {
+        const coachId = rootGetters.userId;
+        return state.requests.filter(req => req.coachId === coachId);
+    },
+    hasRequests(_state, getters) {
+        return getters.requests && getters.requests.length > 0;
+    }
+};
