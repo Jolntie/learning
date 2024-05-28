fff93e967d08b38a54aef4e31a93b89664e23c1d
Jelle <robin@eal.nl>
2024-05-28 15:15:02 +0200

    Optimize webapp speed

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 
 import router from './router.js';
 import store from './store/index.js';
@@ -6,9 +6,10 @@ import App from './App.vue';
 
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
-import BaseBadge from './components/ui/BaseBadge.vue';
 import BaseSpinner from './components/ui/BaseSpinner.vue';
-import BaseDialog from './components/ui/BaseDialog.vue';
+
+const BaseBadge = defineAsyncComponent(() => import('./components/ui/BaseBadge.vue'));
+const BaseDialog = defineAsyncComponent(() => import('./components/ui/BaseDialog.vue'));
 
 const app = createApp(App);
 
